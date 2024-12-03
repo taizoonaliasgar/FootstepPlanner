9de9ecdf7686c4d0598014d2ef8e3b09d21d10a3
taizoonaliasgar <taizoonac@vt.edu>
2024-12-03 18:35:42 -0500

    Raibert fails at 0.8m/s

--- src/A1_up_walk_Raib.cpp
+++ src/A1_up_walk_Raib.cpp
@@ -562,7 +562,7 @@ int main(int argc, char *argv[]) {
     //std::string directory = "/home/taizoon/raisimEnv/raisimWorkspace/footstep_planner/datalog/Oct10/";
     std::string directory = "../datalog/Dec3/";
     // std::string filename = "Payload_Inplace";
-    std::string filename = "upright_A1R_5";
+    std::string filename = "upright_A1R_6";
     // std::string filename = "inplace_sim";
 
 
