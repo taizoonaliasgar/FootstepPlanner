7f957b7de1e3b2f6c41008b5d57e3deedc744241
taizoonaliasgar <taizoonac@vt.edu>
2025-02-16 18:40:08 -0500

    Sim IC8

--- src/LocoWrapper.cpp
+++ src/LocoWrapper.cpp
@@ -161,40 +161,50 @@ void LocoWrapper::calcTau2(const double q[18], const double dq[18], const double
         
         }else{
 
-            // PP->planTraj(state, kin, conEst, gait, phaseVar, ctrlTick, &motion_params, opt_HLstate, NLstep);  
-            // VC->updateVirtualConstraintswalk(state, kin, traj, con, gait, flphase,rlphase, &motion_params, ll);    // update VC's    
-            // VC->setDesiredForce(opt_HLstate.block(12,0,12,1));
-            // z.block(6,0,3*(4-con->cnt),0) += vcon->y.block(6,0,3*(4-con->cnt),0)/ctrlHz;         
-            // LL->calcTorquewalk(state, dyn, kin, vcon, con, &ll_params, HRai, z, Ki); 
-            
-            if( ctrlTick == switchtime*ctrlHz+settlestep*(shifttime2+movetime3) || ctrlTick == switchtime*ctrlHz+settlestep*(shifttime2+movetime3)+shifttime2){
+            if (gait!=gaitTemp || (phaseVar>maxPhase) || ctrlTick == switchtime*ctrlHz){ 
                 locoTick = 0;
-                phaseVar = 0;
-                PP->setToeInit(kin);
-                PP->setx0y0z0(state->q(0),state->q(1),state->q(2),state->q(4));
+                z = Eigen::MatrixXd::Zero(12,1);
+                conEst->forceDomChange();
             }
+            
+            phaseVar = getPhase(1.0*locoTick, 0.0, 199);
+            flphase = phaseVar;
+            rlphase = phaseVar;           
 
-            if(ctrlTick < switchtime*ctrlHz + settlestep*(shifttime2+movetime3)+shifttime2){
-                PP->shiftCoM3(conEst,phaseVar,shifttime2,true);
-                quad->updateSwingMatrices(con->ind,con->cnt);                                               // update the jacobian    
-                VC->updateVirtualConstraints(state, kin, traj, con, gait, phaseVar, &motion_params, ll);    // update VC's
-                VC->setDesiredForce(opt_HLstate.block(12,0,12,1));
-                LL->calcTorquewalk(state, dyn, kin, vcon, con, &ll_params, Hr, z, Ki);
-            }else{
-                if(settlestep%2==0){
-                    nextContact[0] = 0;
-                    nextContact[1] = 1;
-                }else{
-                    nextContact[0] = 1;
-                    nextContact[1] = 0;
-                }
-                conEst->setDesDomain(nextContact);
-                PP->movefoot3(movetime3);
-                quad->updateSwingMatrices(con->ind,con->cnt);
-                VC->updateVirtualConstraintssetfoot(state, kin, traj, con, gait, phaseVar, &motion_params, ll, flphase,rlphase,true);
-                VC->setDesiredForce(opt_HLstate.block(12,0,12,1));
-                LL->calcTorquewalk(state, dyn, kin, vcon, con, &ll_params, Hr, z, Ki);
-            }
+            PP->planTraj(state, kin, conEst, gait, phaseVar, ctrlTick, &motion_params, opt_HLstate, NLstep);  
+            VC->updateVirtualConstraintswalk(state, kin, traj, con, gait, flphase,rlphase, &motion_params, ll);    // update VC's    
+            VC->setDesiredForce(opt_HLstate.block(12,0,12,1));
+            z.block(6,0,3*(4-con->cnt),0) += vcon->y.block(6,0,3*(4-con->cnt),0)/ctrlHz;         
+            LL->calcTorquewalk(state, dyn, kin, vcon, con, &ll_params, HRai, z, Ki); 
+            
+            // if( ctrlTick == switchtime*ctrlHz+settlestep*(shifttime2+movetime3) || ctrlTick == switchtime*ctrlHz+settlestep*(shifttime2+movetime3)+shifttime2){
+            //     locoTick = 0;
+            //     phaseVar = 0;
+            //     PP->setToeInit(kin);
+            //     PP->setx0y0z0(state->q(0),state->q(1),state->q(2),state->q(4));
+            // }
+
+            // if(ctrlTick < switchtime*ctrlHz + settlestep*(shifttime2+movetime3)+shifttime2){
+            //     PP->shiftCoM3(conEst,phaseVar,shifttime2,true);
+            //     quad->updateSwingMatrices(con->ind,con->cnt);                                               // update the jacobian    
+            //     VC->updateVirtualConstraints(state, kin, traj, con, gait, phaseVar, &motion_params, ll);    // update VC's
+            //     VC->setDesiredForce(opt_HLstate.block(12,0,12,1));
+            //     LL->calcTorquewalk(state, dyn, kin, vcon, con, &ll_params, Hr, z, Ki);
+            // }else{
+            //     if(settlestep%2==0){
+            //         nextContact[0] = 0;
+            //         nextContact[1] = 1;
+            //     }else{
+            //         nextContact[0] = 1;
+            //         nextContact[1] = 0;
+            //     }
+            //     conEst->setDesDomain(nextContact);
+            //     PP->movefoot3(movetime3);
+            //     quad->updateSwingMatrices(con->ind,con->cnt);
+            //     VC->updateVirtualConstraintssetfoot(state, kin, traj, con, gait, phaseVar, &motion_params, ll, flphase,rlphase,true);
+            //     VC->setDesiredForce(opt_HLstate.block(12,0,12,1));
+            //     LL->calcTorquewalk(state, dyn, kin, vcon, con, &ll_params, Hr, z, Ki);
+            // }
 
         }
 
