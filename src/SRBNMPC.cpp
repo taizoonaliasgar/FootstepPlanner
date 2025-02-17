a7c93ce068d121abaaa899b5503ae1db4a46cf17
taizoonaliasgar <taizoonac@vt.edu>
2025-02-17 16:22:32 -0500

    Sim IC 10

--- src/SRBNMPC.cpp
+++ src/SRBNMPC.cpp
@@ -1246,9 +1246,9 @@ casadi::DM SRBNMPC::getprevioussol_fullsim(Eigen::Matrix<double,16,1> q0, Eigen:
             
         }
         
-        //if(controlTick==2740){
+        if(controlTick==2740){
             first_time_here = false;
-        //}
+        }
         
     }else{
         //x0(casadi::Slice(12,16)) = previous_sol(casadi::Slice(28,32));
