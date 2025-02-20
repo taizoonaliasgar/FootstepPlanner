3003a30b83820591857c5ccc3654b08fa5d83a1e
taizoonaliasgar <taizoonac@vt.edu>
2025-02-20 17:50:03 -0500

    Sim18: Stabilized hip angle for Kp 50 with Kd 5

--- src/A1_Experiment_Script.cpp
+++ src/A1_Experiment_Script.cpp
@@ -681,7 +681,7 @@ int main(int argc, char *argv[]) {
     double simlength = 50000;//60000;//300*ctrlHz;   // Sim end time
     double fps = 30;            
     //std::string directory = "/home/taizoon/raisimEnv/raisimWorkspace/footstep_planner/datalog/Oct10/";
-    std::string directory = "../data25/Feb16/";
+    std::string directory = "../data25/Feb20/";
     // std::string filename = "Payload_Inplace";
     std::string filename = "fullsim";//"JacVCL_OWCL_rt55_3";
     // std::string filename = "inplace_sim";
