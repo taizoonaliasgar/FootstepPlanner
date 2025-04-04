0778d9b239c307c921ce2783ce68377865c13563
Ruturaj <ruturajsambhus@vt.edu>
2025-04-04 19:14:09 -0400

    Tracks 0.4m/s with estimator on Ruturaj's computer in Multithreaded Sim.

--- include/SRBNMPC.hpp
+++ include/SRBNMPC.hpp
@@ -272,7 +272,7 @@ private:
 
     //Hardware
     std::string lib_name_exp = "/home/trec/WorkRaj/raisim_legged/FootstepPlanner/build/take2_w0p2N20.so";//take2_w0p2N12.so";
-    casadi::Dict opts = {{"ipopt.print_level", 1}, {"print_time", 0},{"ipopt.max_iter", 6},{"ipopt.acceptable_tol", 1e-2},{"ipopt.acceptable_obj_change_tol", 1e-2}};
+    casadi::Dict opts = {{"ipopt.print_level", 1}, {"print_time", 0},{"ipopt.max_iter", 5},{"ipopt.acceptable_tol", 1e-2},{"ipopt.acceptable_obj_change_tol", 1e-2}};
     casadi::Function solver_exp = casadi::nlpsol("solver", "ipopt", lib_name_exp, opts);
 
     std::map<std::string, casadi::DM> argHW, resHW;
