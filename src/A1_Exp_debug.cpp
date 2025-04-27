c6fec296866fb9b18a6312f1b0c1e3a5f1d1210a
taizoonaliasgar <taizoonac@vt.edu>
2025-04-27 18:27:26 -0400

    Apr27Exp4: Robot goes beyond the 20 second mark.

--- src/A1_Exp_debug.cpp
+++ src/A1_Exp_debug.cpp
@@ -195,7 +195,7 @@ public:
     size_t duration = 1.8*ctrlHz;                   // Stand up
     size_t loco_start = settling + duration;        // Start the locomotion pattern
     timer tset;
-	double switchtime = 18;
+	double switchtime = 20;
 	int robotdown = 1;
 	
     Eigen::VectorXd jointTorqueFF = Eigen::MatrixXd::Zero(TOTAL_DOF,1);
