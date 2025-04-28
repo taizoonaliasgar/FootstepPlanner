aab29247ecd592fd2994f2eb592c4cb87c74dc62
taizoonaliasgar <taizoonac@vt.edu>
2025-04-28 15:33:26 -0400

    Apr28Exp4: Rear hip still rotates. Kicked the drawer to tip it.

--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -244,7 +244,7 @@ void LowLevelCtrl::calcTorquewalk(const StateInfo *state, const DynInf *dyn, con
     if(uprighty2){
         for(int i=2; i<4; ++i){
             if (con->ind[i]==1){
-                tau[6+3*i] += -pow(1,i)*30*state->q(6+3*i,0)-2*state->dq(6+3*i,0);//(ll.q.block(6+3*i,0,3,1)-state->q.block(6+3*i,0,3,1));
+                tau[6+3*i] += -pow(1,i)*30*state->q(6+3*i,0)-0.1*state->dq(6+3*i,0);//(ll.q.block(6+3*i,0,3,1)-state->q.block(6+3*i,0,3,1));
                                        //+ 1*(ll.dq.block(6+3*i,0,3,1)-state->dq.block(6+3*i,0,3,1));
             }
         }
