bc6ed93276852d40b69e879d4a37b9bc654c022e
taizoonaliasgar <taizoonac@vt.edu>
2025-05-02 14:47:24 -0400

    Sim1. Reached 26s. Kp=40.

--- src/A1_Sim_MIP.cpp
+++ src/A1_Sim_MIP.cpp
@@ -221,7 +221,7 @@ public:
     std::string name = directory+filename+"_"+".mp4";
     
     double startTime = 0*ctrlHz;    // Recording start time
-    double simlength = 6*ctrlHz;
+    double simlength = 36*ctrlHz;
 
     //Estimator
     int rearweight_est = 4;
--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -575,10 +575,11 @@ void LowLevelCtrl::constraintswalk(LLP *params, const DynInf *dyn, const KinInf 
     double mu = params->mu;
     double kpGain = params->kp;
     if(uprighty){
-        kpGain = 400;
-    }else if(upKp){
-        kpGain = 400;
+        kpGain = 700;
     }
+    // else if(upKp){
+    //     kpGain = 400;
+    // }
     double kdGain = params->kd;
 
     /*std::cout<< "mu" << mu << std::endl;
