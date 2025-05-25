d64b778b346e280dc6a2b2e35be52be6b66cefb8
taizoonaliasgar <taizoonac@vt.edu>
2025-05-25 19:25:28 -0400

    May25Sim3: Failed because the xcom was too much in front.

--- src/LocoWrapper.cpp
+++ src/LocoWrapper.cpp
@@ -135,7 +135,7 @@ void LocoWrapper::calcTau2(const double q[18], const double dq[18], const double
         if(readytowalkf){
         
             // if (gait!=gaitTemp || (phaseVar>maxPhase) || ctrlTick == switchtime*ctrlHz + 2*(shifttime2+movetime3) + shifttime2){ 
-            if (HLMTphase == 0 || HLMTphase == 20 || ctrlTick == switchtime*ctrlHz + 6*(shifttime2+movetime3) + shifttime2){ 
+            if (HLMTphase == 0 || HLMTphase == 20 || ctrlTick == switchtime*ctrlHz + 18*(shifttime2+movetime3) + shifttime2){ 
 
                 locoTick = 0;
                 phaseVar = getPhase(1.0*locoTick, 0.0, 199);
@@ -364,7 +364,7 @@ void LocoWrapper::setfinalCoM2(int settlesteps){
     double rhipz = (kin->hipPos(2,2) + kin->hipPos(2,3))/2;
     Eigen::Matrix<double,4,1> CoM_final = Eigen::MatrixXd::Zero(4,1);
     if(settlesteps>4){
-        CoM_final(0) = (kin->toePos(0,2)+kin->toePos(0,3))/2+(settlesteps-4)*0.1;
+        CoM_final(0) = (kin->toePos(0,2)+kin->toePos(0,3))/2+std::floor((settlesteps-4)/2)*0.05;
     }else{
         CoM_final(0) = (kin->toePos(0,2)+kin->toePos(0,3))/2;
     }
@@ -631,7 +631,7 @@ void LocoWrapper::ExpWrapper(const double jpos_est[18], const double jvel_est[18
 
         if(control_Tick == switchtime*ctrlHz){readytowalk();}//break;}
         if(control_Tick == switchtime*ctrlHz + stepind2_e*(shifttime2+movetime3)){setfinalCoM2(stepind2_e);}//break;}
-        if(control_Tick == switchtime*ctrlHz+6*(shifttime2+movetime3)+shifttime2){startwalking();}//break;}
+        if(control_Tick == switchtime*ctrlHz+18*(shifttime2+movetime3)+shifttime2){startwalking();}//break;}
         // if(control_Tick == (switchtime+6)*ctrlHz){readytoreallywalk();}//break;}
         // if(control_Tick == (switchtime+7)*ctrlHz){readytoreallywalk();}//break;}
 
--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -575,10 +575,10 @@ void LowLevelCtrl::constraintswalk(LLP *params, const DynInf *dyn, const KinInf 
     double mu = params->mu;
     double kpGain = params->kp;
     if(uprighty){
-        kpGain = 700;
+        kpGain = 400;
     }
     else if(upKp){
-        kpGain = 700;
+        kpGain = 400;
     }
     double kdGain = params->kd;
 
