c07c75d413854f20387338cb9e145131e24f8819
taizoonaliasgar <taizoonac@vt.edu>
2025-05-26 14:52:16 -0400

    May26Seq1: Robot falls with NMPC in loop for t>40.

--- src/MotionPlanner.cpp
+++ src/MotionPlanner.cpp
@@ -412,15 +412,17 @@ void MotionPlanner::planTraj(const StateInfo *state, const KinematicsInfo *kin, 
         traj.comDes.block(9,0,3,1) = desOmegaWorld;      
 
     }else if(gait == UPWALK){
-        traj.comDes.block(0,0,3,1) << state->q.block(0,0,3,1);// + opt_HLstate.block(3,0,3,1)*dt;
+        // traj.comDes.block(0,0,3,1) << state->q.block(0,0,3,1);// + opt_HLstate.block(3,0,3,1)*dt;
         // traj.comDes(0) = xnew;
         // traj.comDes(1) = 0;
         traj.comDes(2) = 0.5;//params->standHeight;
-        if(ctrlTick>=50000){
+        if(ctrlTick>=40000){
+            traj.comDes.block(0,0,2,1) << state->q.block(0,0,2,1) + opt_HLstate.block(3,0,2,1)*dt;
             traj.comDes.block(3,0,3,1) = opt_HLstate.block(3,0,3,1);
             traj.comDes.block(6,0,3,1) = opt_HLstate.block(6,0,3,1);
             traj.comDes.block(9,0,3,1) = opt_HLstate.block(9,0,3,1);
         }else{
+            traj.comDes.block(0,0,2,1) << state->q.block(0,0,2,1);// + opt_HLstate.block(3,0,3,1)*dt;
             traj.comDes.block(3,0,3,1) = Eigen::MatrixXd::Zero(3,1);//opt_HLstate.block(3,0,3,1);
             traj.comDes.block(6,0,3,1) = Eigen::MatrixXd::Zero(3,1);//opt_HLstate.block(6,0,3,1);
             traj.comDes.block(9,0,3,1) = Eigen::MatrixXd::Zero(3,1);//opt_HLstate.block(9,0,3,1);
