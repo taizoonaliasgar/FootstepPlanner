5be14a979045e575bec93a277857de087df71a25
taizoonaliasgar <taizoonac@vt.edu>
2025-05-28 11:08:31 -0400

    May27Exp5: Reduced Kp to 200 for a less noisy torque profile.

--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -575,7 +575,7 @@ void LowLevelCtrl::constraintswalk(LLP *params, const DynInf *dyn, const KinInf 
     double mu = params->mu;
     double kpGain = params->kp;
     if(uprighty){
-        kpGain = 400;
+        kpGain = 200;
     }
     else if(upKp){
         kpGain = 400;
