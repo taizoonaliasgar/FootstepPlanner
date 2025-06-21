61276ff8a5c232206b2468272fa48dc6978fcf6e
taizoonaliasgar <taizoonac@vt.edu>
2025-06-21 17:10:54 -0400

    Jun21Sim4: Increased Kp to 700. Does not work.

--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -576,7 +576,7 @@ void LowLevelCtrl::constraintswalk(LLP *params, const DynInf *dyn, const KinInf 
     double kpGain = params->kp;
     double kdGain = params->kd;
     if(uprighty){
-        kpGain = 400;
+        kpGain = 700;
         // kdGain = 5;
     }
     else if(upKp){
