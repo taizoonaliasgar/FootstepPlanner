9f080ab62a57629cb920fe824b8367e3e2ecfd8e
taizoonaliasgar <taizoonac@vt.edu>
2025-07-21 18:39:58 -0400

    Jul21Sim8: Making ydes=0 did not work.

--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -576,7 +576,7 @@ void LowLevelCtrl::constraintswalk(LLP *params, const DynInf *dyn, const KinInf 
     double kpGain = params->kp;
     double kdGain = params->kd;
     if(uprighty){
-        kpGain = 500;
+        kpGain = 600;
         // kdGain = 10;
     }
     else if(upKp){
