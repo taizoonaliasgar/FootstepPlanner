08b75ea70c38e394c6292c9edd7f4bc926e4130f
taizoonaliasgar <taizoonac@vt.edu>
2025-08-13 19:02:25 -0400

    Aug13Exp12: Tried increasing the y Kd gain to 3.

--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -587,7 +587,7 @@ void LowLevelCtrl::constraintswalk(LLP *params, const DynInf *dyn, const KinInf 
         kpGainx = 150;
         kpGainy = 40;
         kdGainx = 5;
-        kdGainy = 2;
+        kdGainy = 3;
     }
     else if(upKp){
         kpGain = 400;
