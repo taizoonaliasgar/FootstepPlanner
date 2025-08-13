fb680aa95167050d2678ac8143506e5eb55aabb7
taizoonaliasgar <taizoonac@vt.edu>
2025-08-13 17:57:38 -0400

    Aug13Exp11: Increased Kd to 2. Improved results.

--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -587,7 +587,7 @@ void LowLevelCtrl::constraintswalk(LLP *params, const DynInf *dyn, const KinInf 
         kpGainx = 150;
         kpGainy = 40;
         kdGainx = 5;
-        kdGainy = 1;
+        kdGainy = 2;
     }
     else if(upKp){
         kpGain = 400;
