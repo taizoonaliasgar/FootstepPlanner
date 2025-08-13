2d68bde60097ffa3e852ea9c0472da37564f1f21
taizoonaliasgar <taizoonac@vt.edu>
2025-08-13 17:47:47 -0400

    Aug13Exp10: Reduced y gains. Vibrations are back.

--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -585,9 +585,9 @@ void LowLevelCtrl::constraintswalk(LLP *params, const DynInf *dyn, const KinInf 
         kpGain = 150;
         kdGain = 5;
         kpGainx = 150;
-        kpGainy = 75;
+        kpGainy = 40;
         kdGainx = 5;
-        kdGainy = 2;
+        kdGainy = 1;
     }
     else if(upKp){
         kpGain = 400;
