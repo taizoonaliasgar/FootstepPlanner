66621e1fa5c632893bdd336ade3713fae2c6388d
taizoonaliasgar <taizoonac@vt.edu>
2025-08-15 16:32:46 -0400

    Aug15Sim5: Reducing x I-O to 75,5 with Raibert did not work.

--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -584,7 +584,7 @@ void LowLevelCtrl::constraintswalk(LLP *params, const DynInf *dyn, const KinInf 
     if(uprighty){
         kpGain = 400;
         // kdGain = 10;
-        kpGainx = 100;
+        kpGainx = 75;
         kpGainy = 100;
         kdGainx = 5;
         kdGainy = 10;
