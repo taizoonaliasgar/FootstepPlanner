6b0a8ce0e6992915fd8a0fd39b92c0e1e90e4c9e
taizoonaliasgar <taizoonac@vt.edu>
2025-08-21 11:11:31 -0400

    Aug21Sim2: Changed to A1 optimum gains. Did not work.

--- src/SRBNMPC.cpp
+++ src/SRBNMPC.cpp
@@ -134,7 +134,7 @@ void SRBNMPC::generator(){
     //casadi::Function solver = casadi::nlpsol("solver", "ipopt", {{"x", x}, {"f", f}, {"g", g}, {"p", p}});//, opts);
     casadi::Function solver = casadi::nlpsol("solver", "ipopt", nlp_prob, opts);
     // file name
-    std::string file_name = "Go2_w0p2_ro_0p05_1";//"take2_1";
+    std::string file_name = "Go2_w0p2_ro_0p05_2";//"take2_1";
     // code predix
     std::string prefix_code = "/home/taizoon/raisimEnv/Workspace/FootstepPlanner/build/";//std::filesystem::current_path().string() + "/";
 
@@ -288,11 +288,11 @@ casadi::SX SRBNMPC::UpdateCostN(casadi::SX x, casadi::SX x_des){
     
     
     Q.setZero();
-    Q.block(0,0,3,3).diagonal() <<  1e4,5e4,8e5;//5e4//1e4//mpc_params.qpx, mpc_params.qpy, mpc_params.qpz;
+    Q.block(0,0,3,3).diagonal() <<  1e4,1e5,8e5;//5e4//1e4//mpc_params.qpx, mpc_params.qpy, mpc_params.qpz;
     Q.block(3,3,3,3).diagonal() <<  1e5,1e4,1e4;//4e5//5e4//mpc_params.qvx, mpc_params.qvy, mpc_params.qvz;
-    Q.block(6,6,3,3).diagonal() <<  8e4,8e5,3e4;//3e4//mpc_params.qrr, mpc_params.qrp, mpc_params.qry;
+    Q.block(6,6,3,3).diagonal() <<  8e5,8e5,8e5;//3e4//mpc_params.qrr, mpc_params.qrp, mpc_params.qry;
     //Yaw:3e3
-    Q.block(9,9,3,3).diagonal() <<  1e2,1e2,1e2;//1//1e2//mpc_params.qwr, mpc_params.qwp, mpc_params.qwy;
+    Q.block(9,9,3,3).diagonal() <<  5e3,5e3,5e3;//1//1e2//mpc_params.qwr, mpc_params.qwp, mpc_params.qwy;
     Q.block(12,12,4,4).diagonal() <<  1000,1000,1000,1000;//1e-2//1e3//1e5,1e5,1e5,1e5;
     //repdiag(Q,Q_rep,HORIZ+1);
     
