b3a0ba3d08cb88031debbab63765ad953b25bfe6
taizoonaliasgar <taizoonac@vt.edu>
2025-08-25 14:10:47 -0400

    Aug25Sim8: Increasing Q_yaw and front_off to 0.25 did not help.

--- src/MotionPlanner.cpp
+++ src/MotionPlanner.cpp
@@ -417,6 +417,7 @@ void MotionPlanner::planTraj(const StateInfo *state, const KinematicsInfo *kin, 
         // traj.comDes(1) = 0;
         traj.comDes(2) = 0.5;//params->standHeight;
         Eigen::Matrix<double, 3, 1> desOmegaWorld = getthetadot(opt_HLstate.block(6,0,6,1));
+        // Eigen::Matrix<double, 3, 1> desOmegaWorld = toWorld(opt_HLstate.block(9,0,3,1),state->R);
         if(ctrlTick>=37000){
             traj.comDes.block(0,0,2,1) << state->q.block(0,0,2,1) + opt_HLstate.block(3,0,2,1)*dt;
             traj.comDes.block(3,0,3,1) = opt_HLstate.block(3,0,3,1);
