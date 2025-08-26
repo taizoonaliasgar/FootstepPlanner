c12ae16e93074f0cf3e43e0439ede85f585b3caf
taizoonaliasgar <taizoonac@vt.edu>
2025-08-26 11:50:56 -0400

    Aug26Exp5: Reduced Kp to 300. Did not work.

--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -582,9 +582,9 @@ void LowLevelCtrl::constraintswalk(LLP *params, const DynInf *dyn, const KinInf 
     double kdGainy = params->kd; 
 
      if(MPCgains){
-        kpGain = 600; 
-        kpGainx = 600;
-        kpGainy = 600;
+        kpGain = 300; 
+        kpGainx = 300;
+        kpGainy = 300;
 
         kdGain = 20;
         kdGainx = 20;
