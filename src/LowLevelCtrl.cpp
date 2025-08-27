4550e20f266332a88ca60022c640f19f612e15e3
taizoonaliasgar <taizoonac@vt.edu>
2025-08-27 13:18:16 -0400

    Aug26Exp13: Increased dF z to 500. Failed badly.

--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -37,7 +37,7 @@ LowLevelCtrl::LowLevelCtrl(){
 
     c_int exitflag = osqp_setup(&work, data, settings);
     #endif
-    dFMultiplier(0,0)=100;dFMultiplier(1,1)=100;dFMultiplier(2,2)=300;
+    dFMultiplier(0,0)=100;dFMultiplier(1,1)=100;dFMultiplier(2,2)=500;
 }
 
 LowLevelCtrl::~LowLevelCtrl(){
