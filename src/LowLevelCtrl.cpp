3f285959ab0b65d2df2050dac6e8bc818eced51e
taizoonaliasgar <taizoonac@vt.edu>
2025-08-29 13:52:54 -0400

    Aug26Exp29: Ran experiment with MPC.

--- src/LowLevelCtrl.cpp
+++ src/LowLevelCtrl.cpp
@@ -37,7 +37,7 @@ LowLevelCtrl::LowLevelCtrl(){
 
     c_int exitflag = osqp_setup(&work, data, settings);
     #endif
-    dFMultiplier(0,0)=1;dFMultiplier(1,1)=1;dFMultiplier(2,2)=1;
+    dFMultiplier(0,0)=100;dFMultiplier(1,1)=100;dFMultiplier(2,2)=100;
 }
 
 LowLevelCtrl::~LowLevelCtrl(){
