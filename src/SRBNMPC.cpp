448c8d045fa0ee987afdc8756f6c7721e6f60ea0
taizoonaliasgar <taizoonac@vt.edu>
2025-08-29 11:36:14 -0400

    Aug26Exp25: Tried scaling velocity down by 0.1. Did not work.

--- src/SRBNMPC.cpp
+++ src/SRBNMPC.cpp
@@ -1420,7 +1420,7 @@ void SRBNMPC::planner_MT(size_t controlTick, double q[18], double dq[18], Eigen:
         q0_MT(i+6) = q[i+3];
         q0_MT(i+9) = dq[i+3];
     }
-    q0_MT(5)=-0.0;
+    q0_MT(5)=0.1*q0_MT(5);
     //auto end01 = std::chrono::high_resolution_clock::now();
     
     getprevioussol_fullsimMT(q0_MT,foot_position,lastQPforce,controlMPC_MT);
