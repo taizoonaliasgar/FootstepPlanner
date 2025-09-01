f021194656d9006c7f2b4b44e40dd1e205027362
taizoonaliasgar <taizoonac@vt.edu>
2025-08-31 22:06:32 -0400

    Aug26Exp51: Included z from MPC. Height increases again.

--- src/MotionPlanner.cpp
+++ src/MotionPlanner.cpp
@@ -431,7 +431,7 @@ void MotionPlanner::planTraj(const StateInfo *state, const KinematicsInfo *kin, 
             traj.comDes.block(3,0,3,1) = opt_HLstate.block(3,0,3,1);
             traj.comDes.block(6,0,3,1) = 0*opt_HLstate.block(6,0,3,1);
             traj.comDes.block(9,0,3,1) = 0*desOmegaWorld;
-            traj.comDes(2) = 0.5;//opt_HLstate(2);
+            traj.comDes(2) = opt_HLstate(2);
             traj.comDes(7) = 0.35;
         }
         else if(ctrlTick>=37000){
